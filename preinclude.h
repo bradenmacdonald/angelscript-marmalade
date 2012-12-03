c5c7e07e71d954434791082fec0b3bbf1a27ca29
Braden MacDonald <bmacdonald@mstar-ca.com>
2012-12-03 14:29:02 -0800

    Add Windows binary and fix MSVC compilation.

--- preinclude.h
+++ preinclude.h
@@ -7,3 +7,12 @@
 #else
 	#error Unsupported architecture. Fix the preinclude file.
 #endif
+
+// Fix missing fopen_s in Marmalade MSVC environment
+#if defined(_MSC_VER) && _MSC_VER >= 1500 
+	#include <stdio.h>
+	inline int fopen_s(FILE** pFile, const char *filename, const char *mode) {
+		*pFile = fopen(filename, mode);
+		return 0;
+	}
+#endif
